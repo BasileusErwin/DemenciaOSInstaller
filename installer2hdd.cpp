a91a8c4476f9d13e58b722b6ccae452f9611a54c
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-01 10:09:06 +0100

    Added swap option

--- installer2hdd.cpp
+++ installer2hdd.cpp
@@ -7,9 +7,19 @@ using namespace std;
 
 int option;
 string language;
+string swap;
+bool usingSwap;
+void MakeSwap(swap)
+{
+	string cmd = "mkswap " + swap;
+	string cmd2 = "swapon " + swap;
+	system(cmd.c_str());
+	system(cmd2.c_str());
+}
 
 void Install()
 {
+	string swapoption;
     string disk;
     system("clear");
     system("lsblk");
@@ -23,38 +33,51 @@ void Install()
     }
     else {
             try {
+				
                 cout << "Enter to fdisk " + disk << endl;
                 string exec = "cfdisk " + disk;
                 system(exec.c_str());
                 cout << "OK" << endl;
-
-                cout << "Formating partitions" << endl;
-                string exec2 = "mkfs.ext4 " + disk+"1";
-                system(exec2.c_str());
-                cout << disk + "1" + " it's created sucessfully!" << endl;
-                system("mkdir /media/target");
-                cout << "Mounting partitions...." << endl;
-                string exec3 = "mount -t ext4 " + disk+"1" + " /media/target";
-                system(exec3.c_str());
-                cout << "Installing...." << endl;
-                string exec4 = "unsquashfs -f -d /media/target/ /media/cdrom/casper/filesystem.squashfs";
-                system(exec4.c_str());
-                cout << "Installing bootloader (grub)" << endl;
-                string exec5 = "grub-install --target=i386-pc --root-directory=/media/target/ " + disk;
-                system(exec5.c_str());
-                string exec6 = "mount --bind /proc/ /media/target/proc/";
-                string exec10 = "mount --bind /sys/ /media/target/sys/";
-                string exec12 = "mount --bind /dev/ /media/target/dev/";
-                cout << "Write apt install arch-install-scripts -y in other terminal and run genfstab -U /media/target/ >> /media/target/etc/fstab" << endl;
-                cout << "Write update-grub in chroot for make grub config!" << endl;
-                system(exec6.c_str());
-                system(exec10.c_str());
-                system(exec12.c_str());
-                system("chroot /media/target");
-                cout << "Installation complete!" << endl;
-                cout << "The computer restarts now!";
-                system("systemctl reboot");
-            }
+				cout << "You do want use SWAP? (yes/no)" << endl;
+				cin >> swapoption;
+				if (swapoption="yes")
+				{
+					usingSwap=true;
+				}
+				else {
+					usingSwap=false;
+				}
+				
+				cout << "Formating partitions" << endl;
+				string exec2 = "mkfs.ext4 " + disk+"1";
+				system(exec2.c_str());
+				cout << disk + "1" + " it's created sucessfully!" << endl;
+				system("mkdir /media/target");
+				cout << "Mounting partitions...." << endl;
+				string exec3 = "mount -t ext4 " + disk+"1" + " /media/target";
+				system(exec3.c_str());
+				if (usingSwap=true){
+					cout << "Creating swap" << endl;
+					MakeSwap(swap);
+					cout << "Swap created sucessfully" << endl;
+				}
+				cout << "Installing...." << endl;
+				string exec4 = "unsquashfs -f -d /media/target/ /media/cdrom/casper/filesystem.squashfs";
+				system(exec4.c_str());
+				cout << "Installing bootloader (grub)" << endl;
+				string exec5 = "grub-install --target=i386-pc --root-directory=/media/target/ " + disk;
+				system(exec5.c_str());
+				string exec6 = "mount --bind /proc/ /media/target/proc/";
+				string exec10 = "mount --bind /sys/ /media/target/sys/";
+				string exec12 = "mount --bind /dev/ /media/target/dev/";
+				string exec13 = "apt install arch-install-scripts -y || genfstab -U /media/target/ >> /media/target/etc/fstab";
+				cout << "Installing genfstab and generating fstab for the target disk" << endl;
+				system(exec13.c_str());
+				system(exec6.c_str());
+				system(exec10.c_str());
+				system(exec12.c_str());
+				system("chroot /media/target");
+				cout << "Installation complete!" << endl;
             catch (string ex)
             {
                 cout << ex << endl;
